e565bc06d935160709e59ba53e93f0a12fdaf4e6
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-08-28 05:13:29 +0900

    remove unused font fetching

--- 2016/index.php
+++ 2016/index.php
@@ -15,8 +15,6 @@
   <link href="css/box.css" rel="stylesheet">
   <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-  <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
-
   <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
   <link rel="icon" type="image/png" href="img/favicon/favicon-32x32.png" sizes="32x32">
   <link rel="icon" type="image/png" href="img/favicon/favicon-16x16.png" sizes="16x16">
