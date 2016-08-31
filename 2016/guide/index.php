597f96487b9506f24971655e5460e0d5a9037c28
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-09-01 03:35:41 +0900

    rename

--- 2016/guide/index.php
+++ 2016/guide/index.php
@@ -1,4 +1,4 @@
 <?php
 // redirect: guide page was moved to the top directory.
-header('Location: http://vocacon.net/2016/guides.html');
+header('Location: http://vocacon.net/2016/guides.php');
 exit;
